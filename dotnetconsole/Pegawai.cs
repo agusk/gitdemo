4dbffb7b4c85535edfd7ba1b8679887d1ebdfd26
Agus Kurniawan <agusk@users.noreply.github.com>
2023-04-05 09:39:25 +0700

    tambah kelas pegawai

---
+++ dotnetconsole/Pegawai.cs
@@ -0,0 +1,9 @@
+public class Pegawai
+{
+    public int Id { get; set; }
+    public string Nama { get; set; }
+    public string Alamat { get; set; }
+    public string Jabatan { get; set; }
+    public string Email { get; set; }
+    public string NoHp { get; set; }
+}

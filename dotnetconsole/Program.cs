2863899437fb4dd6d1379f31e5b9eab69c99eb20
Agus Kurniawan <agusk@users.noreply.github.com>
2023-04-05 09:40:09 +0700

    updated

--- dotnetconsole/Program.cs
+++ dotnetconsole/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Hello, World!");
 
 // ini benar
 Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, World! 3");

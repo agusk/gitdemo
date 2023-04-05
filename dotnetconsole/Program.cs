78faec31eb4e16ca10344f8a83e87ff6a7fd5fc0
Agus Kurniawan <agusk@users.noreply.github.com>
2023-04-05 09:17:48 +0700

    updated

--- dotnetconsole/Program.cs
+++ dotnetconsole/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("Hello, World!");
 
 // ini benar
 Console.WriteLine("Hello, World! 3");
+
+Product o = new Product{Id=1, Name="Product 1", Description="Product 1 Description", Price=1000, Category="Category 1"};
